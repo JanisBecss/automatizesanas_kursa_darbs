0ac9d1de0f3bae066d25e1e5596573a6977266ae
JanisBecss <Janis.Becs_1@edu.rtu.lv>
2023-12-27 01:56:31 +0200

    dd

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ KURSA DARBS
 Jānis Bečs 231RDB015
 
 Projekta uzdevums:
+https://www.myfitnesspal.com/nutrition-facts-calories/cereals
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello")
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+from openpyxl import Workbook, load_workbook 
+
+service = Service()
+option = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=option)
+
+
+time.sleep(2)
