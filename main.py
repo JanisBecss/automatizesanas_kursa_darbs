610b3107e929cfee5bc1a57ccfc1a59fedbd087a
JanisBecss <Janis.Becs_1@edu.rtu.lv>
2024-01-11 18:11:35 +0200

    11.01.2024

--- main.py
+++ main.py
@@ -11,21 +11,28 @@ import translate
 
 edieni=[]
 svars_g=[]
+kalorijas = tauki = oglhidrati = proteini = 36
+# kalorijas, tauki, oglhidrati, proteini = search.mekletajs("apple") # g uz 100g produkta 
 
 
-kalorijas, tauki, oglhidrati, proteini = search.mekletajs("apple") # g uz 100g produkta 
+# while True:
+#     ediens_lv = str(input("Ko ēdāt: "))
+#     if ediens_lv.lower() == "x":   
+#         break
+#     edieni.append(ediens_lv)
+#     svars = float(input("Cik g apēdāt: "))
+#     svars_g.append(svars) # ēdienu svari (g) saglabāti list
 
+# edieni_eng = translate.tulkosana(edieni) # ēdienu angļu tulkojumi saglabāti list
 
-while True:
-    ediens_lv = str(input("Ko ēdāt: "))
-    if ediens_lv.lower() == "x":   
-        break
-    edieni.append(ediens_lv)
-    svars = float(input("Cik g apēdāt: "))
-    svars_g.append(svars) # ēdienu svari (g) saglabāti List
+# for i in range(len(edieni_eng)):
+#     kalorijas, tauki, oglhidrati, proteini = search.mekletajs(edieni_eng[i]) # g uz 100g produkta 
+#     kalorijas += kalorijas
+#     tauki += tauki
+#     oglhidrati += oglhidrati
+#     proteini += proteini 
 
-edieni_eng = translate.tulkosana(edieni) # ēdinu angļu tulkojumi saglabāti list
-
+print(kalorijas, tauki, oglhidrati, proteini)
 
 
 
